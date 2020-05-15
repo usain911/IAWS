831944806dc20a470af04a40c1991504ea340b13
usain <jens_boldt@gmx.de>
2020-05-15 12:19:14 +0200

    ein paar Componenten hinzugefügt und der Aufbau sollte so etwas verständlicher sein

--- projekt/src/app/app-routing.module.ts
+++ projekt/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ListProjectsComponent } from './list-projects/list-projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: ListProjectsComponent },
+  { path: 'project', component: ProjectDetailsComponent },
+  { path: 'register', component: RegisterUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projekt/src/app/app.module.ts
+++ projekt/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { ListProjectsComponent } from './list-projects/list-projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListProjectsComponent,
+    ProjectDetailsComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
