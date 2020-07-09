57bf6c4de22fe43f0d5b9ef26ca71b317f5aa07a
AhmetKocaker <kocaker.ahmet@gmx.de>
2020-07-09 12:09:27 +0200

    Neue Variable

--- projekt/server/DataBaseAPI/DataBaseAPI/Models/Teilaufgaben.cs
+++ projekt/server/DataBaseAPI/DataBaseAPI/Models/Teilaufgaben.cs
@@ -15,13 +15,14 @@ namespace DataBaseAPI.Models
         public string Beschreibung { get; set; }
         public int? ErstellerId { get; set; }
         public int Zugewiesen { get; set; }
-        public TimeSpan? SollZeit { get; set; }
-        public TimeSpan? IstZeit { get; set; }
         public bool? Erledigt { get; set; }
         public DateTime? ErstellDatum { get; set; }
         public int? ZugeordnetZuAufgabe { get; set; }
         public int? VorgaengerId { get; set; }
         public int? NachfolgerId { get; set; }
+        public double? IstZeit { get; set; }
+        public double? SollZeit { get; set; }
+        public DateTime? Deadline { get; set; }
 
         public virtual ICollection<NutzerAufgaben> NutzerAufgaben { get; set; }
     }
